5eedfd60cbc58089ee44d796619ddbcefbffe30b
aksource <akira.mew@gmail.com>
2016-06-16 23:42:29 +0900

    test

--- java/StoneCutter/StoneCutter.java
+++ java/StoneCutter/StoneCutter.java
@@ -12,32 +12,41 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
-@Mod(modid="StoneCutter", name="StoneCutter", version="@VERSION@",dependencies="required-after:FML")
-//@NetworkMod(clientSideRequired=true, serverSideRequired=false)
-public class StoneCutter
-{
+@Mod(modid = StoneCutter.MOD_ID,
+        name = StoneCutter.MOD_NAME,
+        version = StoneCutter.MOD_VERSION,
+        dependencies = StoneCutter.MOD_DEPENDENCIES,
+        useMetadata = true,
+        acceptedMinecraftVersions = StoneCutter.MOD_MC_VERSION)
+public class StoneCutter {
     public static final String MOD_ID = "StoneCutter";
-	@Mod.Instance("StoneCutter")
-	public static StoneCutter instance;
-	public static Item cutter;
-	@Mod.EventHandler
-	public void preinit(FMLPreInitializationEvent event)
-	{
-		//アイテムの登録はpreInitで行う必要あり。
-		cutter = (new ItemStoneCutter()).setUnlocalizedName("cutter")/*.setTextureName("shears")*/.setCreativeTab(CreativeTabs.tabTools);
-		//1.7以降はこのメソッドで登録。
-		GameRegistry.registerItem(cutter, "stone_cutter");
-	}
-	@Mod.EventHandler
-	public void load(FMLInitializationEvent event)
-	{
+    public static final String MOD_NAME = "StoneCutter";
+    public static final String MOD_VERSION = "@VERSION@";
+    public static final String MOD_DEPENDENCIES = "required-after:FML";
+    public static final String MOD_MC_VERSION = "[1.8,1.8.9]";
+
+    public static Item cutter;
+
+    @Mod.EventHandler
+    public void preinit(FMLPreInitializationEvent event) {
+        //アイテムの登録はpreInitで行う必要あり。
+        cutter = (new ItemStoneCutter()).setUnlocalizedName("cutter")/*.setTextureName("shears")*/.setCreativeTab(CreativeTabs.tabTools);
+        //1.7以降はこのメソッドで登録。
+        GameRegistry.registerItem(cutter, "stone_cutter");
+    }
+
+    @Mod.EventHandler
+    public void load(FMLInitializationEvent event) {
         //1.8以降はこのメソッドでテクスチャ/モデルJSONファイルを登録。クライアント側のみの処理
         if (event.getSide().isClient()) {
-            Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(cutter, 0, new ModelResourceLocation(MOD_ID + ":" + "stone_cutter", "inventory"));
+            Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(
+                    cutter,
+                    0,
+                    new ModelResourceLocation(MOD_ID + ":" + "stone_cutter", "inventory"));
         }
-		//onCraftingがイベント処理に変わったので、FMLに登録。
-		FMLCommonHandler.instance().bus().register(cutter);
-		GameRegistry.addShapelessRecipe(new ItemStack(Blocks.stone_slab, 2, 3),
-				new ItemStack(Blocks.cobblestone), new ItemStack(cutter,1,32767));
-	}
+        //onCraftingがイベント処理に変わったので、FMLに登録。
+        FMLCommonHandler.instance().bus().register(cutter);
+        GameRegistry.addShapelessRecipe(new ItemStack(Blocks.stone_slab, 2, 3),
+                new ItemStack(Blocks.cobblestone), new ItemStack(cutter, 1, 32767));
+    }
 }
